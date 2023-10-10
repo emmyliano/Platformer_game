715530c21c9b430e818607fcd58bc61328ec5c15
Akemu Emmanuel <akemuemmy@gmail.com>
2023-10-10 16:37:56 +0100

    Successfully changed the background

--- tutorial.py
+++ tutorial.py
@@ -10,7 +10,7 @@ pygame.display.set_caption("Platformer")
 
 WIDTH, HEIGHT = 1000, 800
 FPS = 60
-PLAYER_VEL = 5
+PLAYER_VEL = 20
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
@@ -48,7 +48,7 @@ def get_block(size):
     path = join("assets", "Terrain", "Terrain.png")
     image = pygame.image.load(path).convert_alpha()
     surface = pygame.Surface((size, size), pygame.SRCALPHA, 32)
-    rect = pygame.Rect(96, 0, size, size)
+    rect = pygame.Rect(0, 0, size, size)
     surface.blit(image, (0, 0), rect)
     return pygame.transform.scale2x(surface)
 
